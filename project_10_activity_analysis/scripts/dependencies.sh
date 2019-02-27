20e5c0bc5fe313ecef51a1f070efeb9bf070af72
FollowJack <d.horvatic@outlook.com>
2019-02-27 10:27:29 +0100

    add scripts for dependencies and start

---
+++ project_10_activity_analysis/scripts/dependencies.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# using venv kernel
+pip install ipykernel;
+ipython kernel install --user --name=venv;
+# dependencies HERE
+pip install numpy;
+pip install pandas;
+pip install matplotlib;
+pip install seaborn;
