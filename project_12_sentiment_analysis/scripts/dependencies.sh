1c08f8cfa55f2f65cab7d89c05119850e64e6ce4
FollowJack <d.horvatic@outlook.com>
2019-04-03 14:05:42 +0200

    Update notebook structure + automatization

--- project_12_sentiment_analysis/scripts/dependencies.sh
+++ project_12_sentiment_analysis/scripts/dependencies.sh
@@ -2,7 +2,4 @@
 # using venv kernel
 pip install ipykernel;
 ipython kernel install --user --name=venv;
-# dependencies HERE
-pip install tweepy;
-pip install textblob;
-pip install flask;
+pip install -r requirements.txt;
