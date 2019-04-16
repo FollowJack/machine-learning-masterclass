d59e1b7106a8fd066722eeb00829db3e7f0fb5f7
FollowJack <d.horvatic@outlook.com>
2019-04-16 23:00:04 +0200

    Add more insights

--- project_16_call_center_nlp/scripts/dependencies.sh
+++ project_16_call_center_nlp/scripts/dependencies.sh
@@ -4,5 +4,8 @@ pip install ipykernel;
 ipython kernel install --user --name=venv;
 pip install -r requirements.txt;
 
+# Necessary for NTLK error
+# see https://stackoverflow.com/questions/41348621/ssl-error-downloading-nltk-data
+/Applications/Python 3.6/Install Certificates.command
 # download required
-# python -m nltk.downloader punkt stopwords reuters maxent_treebank_pos_tagger averaged_perceptron_tagger wordnet
+python -m nltk.downloader punkt stopwords reuters maxent_treebank_pos_tagger averaged_perceptron_tagger wordnet
