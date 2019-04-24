485b112e6322ef2b108bf7829f11a7f21fd53c27
FollowJack <d.horvatic@outlook.com>
2019-04-24 17:54:46 +0200

    Added hottest trends in machine learning project

--- project_13_natural_language_processing/scripts/dependencies.sh
+++ project_13_natural_language_processing/scripts/dependencies.sh
@@ -4,5 +4,6 @@ pip install ipykernel;
 ipython kernel install --user --name=venv;
 pip install -r requirements.txt;
 
+/Applications/Python 3.6/Install Certificates.command
 # download required
 python -m nltk.downloader punkt stopwords reuters maxent_treebank_pos_tagger averaged_perceptron_tagger wordnet
