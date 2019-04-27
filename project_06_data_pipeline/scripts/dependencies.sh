be5869d5e3948bfec527a43b6c15db1cdb8c1c34
FollowJack <d.horvatic@outlook.com>
2019-04-27 16:56:47 +0200

    Updated overall structure

--- project_06_data_pipeline/scripts/dependencies.sh
+++ project_06_data_pipeline/scripts/dependencies.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # using venv kernel
 pip install ipykernel;
+pip install --upgrade ipykernel;
 ipython kernel install --user --name=venv;
 # dependencies HERE
 pip install -r requirements.txt;
