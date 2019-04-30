6c0f2692f91c9c1646e2521b80f42684b84fe92d
FollowJack <d.horvatic@outlook.com>
2019-04-30 11:21:13 +0200

    Added variables

--- project_01_generated_data_basic_panda_and_matplotlib/scripts/pipeline.sh
+++ project_01_generated_data_basic_panda_and_matplotlib/scripts/pipeline.sh
@@ -1,56 +1,31 @@
 #!/bin/bash
+
+########################################
+# Configs
+########################################
+DEV_DOCKER_CONTAINER_NAME="jupyter_dev"
+DEV_DOCKER_IMAGE_NAME="jupyter_lab"
+
+########################################
+# Pyton
+########################################
 function dependencies(){
   echo Install dependencies
-  # upgrade pip
+  # upgrade pip for removing error message of outdated pip
   pip install --upgrade pip
+
   # using venv kernel
   # pip install ipykernel;
   # pip install --upgrade ipykernel;
   # ipython kernel install --user --name=venv;
   # reinstall jupyter notebook
   # pip install --upgrade --force-reinstall --no-cache-dir jupyter;
+
   # install requirements
   pip install -r configs/requirements.txt;
 
   # For SSL problems mostly with tensorflow
   # /Applications/Python 3.5/Install Certificates.command
-}
-
-function docker_build_jupyter(){
-  echo Build Docker jupyter
-  docker build -f dockerfiles/Dockerfile_jupyter . --tag jupyter_dev
-}
-
-function docker_freeze_dependencies(){
-  echo Run freeze dependencies;
-  docker exec -it jupyter_lab sh -c "scripts/pipeline.sh freeze_dependencies";
-}
-
-function docker_run_jupyter(){
-  echo Run Docker jupyter;
-
-  docker run \
-    -p 8888:8888 \
-    --rm \
-    --name jupyter_lab \
-    --mount type=bind,source="$(pwd)"/notebooks,target=/app/notebooks \
-    --mount type=bind,source="$(pwd)"/data,target=/app/data \
-    --mount type=bind,source="$(pwd)"/configs,target=/app/configs \
-    jupyter_dev;
-
-}
-
-function docker_run_terminal(){
-  echo Run Docker terminal; # TODO
-
-  docker run \
-    -p 8888:8888 \
-    --rm \
-    --name jupyter_lab \
-    --mount type=bind,source="$(pwd)"/notebooks,target=/app/notebooks \
-    --mount type=bind,source="$(pwd)"/data,target=/app/data \
-    --mount type=bind,source="$(pwd)"/configs,target=/app/configs \
-    jupyter_dev
 }
 
 function freeze_dependencies(){
@@ -75,6 +50,43 @@ function virtual_env(){
   which python;
 }
 
+########################################
+# Docker
+########################################
+function docker_build_jupyter(){
+  echo Build Docker jupyter
+  docker build -f dockerfiles/Dockerfile_jupyter . --tag ${DEV_DOCKER_CONTAINER_NAME}
+}
+
+function docker_freeze_dependencies(){
+  echo Run freeze dependencies;
+  docker exec -it ${DEV_DOCKER_IMAGE_NAME} sh -c "scripts/pipeline.sh freeze_dependencies";
+}
+
+function docker_run_jupyter(){
+  echo Run Docker jupyter;
+
+  docker run \
+    -p 8888:8888 \
+    --rm \
+    --name ${DEV_DOCKER_IMAGE_NAME} \
+    --mount type=bind,source="$(pwd)"/notebooks,target=/app/notebooks \
+    --mount type=bind,source="$(pwd)"/data,target=/app/data \
+    --mount type=bind,source="$(pwd)"/configs,target=/app/configs \
+    ${DEV_DOCKER_CONTAINER_NAME};
+
+}
+
+function docker_run_terminal(){
+  echo Run Docker terminal;
+  echo TODO: Implement feature # TODO
+}
+
+
+
+########################################
+# Set methods public
+########################################
 case $1 in
   docker_build_jupyter)
     docker_build_jupyter
